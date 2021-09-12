8a555ef82822454c25b1de85a8038ba3a0de3de1
ryepc827 <82628948+ryepc827@users.noreply.github.com>
2021-09-11 21:19:02 -0700

    Add files via upload

---
+++ date.go
@@ -0,0 +1,55 @@
+package calendar
+
+import "errors"
+
+type Date struct {
+	Year int
+	Month int
+	Day   int
+}
+
+func (d *Date) SetYear(year int) error {
+	if year < 1 {
+		return errors.New("invalid year")
+	}
+	d.Year = year
+	return nil
+}
+
+func (d *Date) SetMonth(month int) error {
+	if month < 1 || month > 12 {
+		return errors.New("invalid month")
+	}
+	d.Month = month
+	return nil
+}
+
+func (d *Date) SetDay(day int) error {
+	if day < 1 || day > 31 {
+		return errors.New("invalid day")
+	}
+	d.Day = day
+	return nil
+}
+
+func main() {
+	date := Date{}
+	//err := date.SetYear(2019)
+	//date.SetYear(2019)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//date.SetMonth(5)
+	//err :=date.SetMonth(14)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//date.SetDay(27)
+	err := date.SetDay(30)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(date.Year)
+	fmt.Println(date.Month)
+	fmt.Println(date.Day)
+}
